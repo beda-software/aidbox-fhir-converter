3bec5ca5d8f9c49a0e34249e605ae57498350f8e
Dmitry Shutov <dmitry.s@beda.software>
2025-05-23 12:24:36 +0500

    Add $to-format/aidbox and $to-format/fhir operations

--- src/operations.ts
+++ src/operations.ts
@@ -1,147 +1,41 @@
 import * as assert from "assert";
 import { NotFoundError, ValidationError } from "@aidbox/node-server-sdk";
 import { TOperation } from "./helpers";
+import { fromFirstClassExtension, toFirstClassExtension } from "sdc-qrf";
+import { Questionnaire, QuestionnaireResponse } from "fhir/r4b";
+import {
+  Questionnaire as FCEQuestionnaire,
+  QuestionnaireResponse as FCEQuestionnaireResponse,
+} from "@beda.software/aidbox-types";
 
-export const createPatient: TOperation<{
+export const toAidboxFormat: TOperation<{
   // Typing "resource" (POST payload)
-  resource: {
-    name: string;
-    active: boolean;
-  };
-  // Optionally typing query/route params & form payload
-  params: {
-    foo: string;
-  };
-  "form-params": {
-    foo: string;
-  };
-  "route-params": {
-    foo: string;
-  };
+  // resource: Questionnaire | QuestionnaireResponse;
+  resource: any;
 }> = {
   method: "POST",
-  path: ["createPatient"],
+  path: ["$to-format", "aidbox"],
   handlerFn: async (req, { ctx }) => {
-    const {
-      // "resource" contains POST payload
-      resource,
-      // "params", "form-params" & "route-params" are also accessible
-      params,
-      "form-params": formParams,
-      "route-params": routeParams,
-    } = req;
+    const { resource } = req;
     assert.ok(resource, new ValidationError("resource required"));
-    const { active, name } = resource;
 
-    assert.ok(
-      typeof active !== "undefined",
-      new ValidationError('"active" required')
-    );
-    assert.ok(name, new ValidationError('"name" required'));
+    const fceResource = toFirstClassExtension(resource);
 
-    const patient = await ctx.api.createResource<any>("Patient", {
-      active: active,
-      name: [{ text: name }],
-    });
-    return { resource: patient };
+    return { resource: {resource: fceResource}, status: 200 };
   },
 };
 
-export const test: TOperation<{ resource: { active: boolean } }> = {
-  method: "GET",
-  path: ["test"],
-  handlerFn: async (req, { ctx, helpers }) => {
-    // Test helpers
-    console.log("Testing helpers");
-    const { resources: patients, total: patientsTotal } =
-      await helpers.findResources<any>("Patient", {
-        _sort: "-createdAt",
-        _count: 3,
-      });
-    console.log({ patientsTotal, patients });
-
-    // Test log
-    console.log("Testing log");
-    await ctx.log({
-      message: { error: "Testing log" },
-      v: "2020.02",
-      fx: "testOperation",
-      type: "backend-test",
-    });
-
-    return { status: 200 };
-  },
-};
-
-export const testError: TOperation<{ params: { type: string } }> = {
-  method: "GET",
-  path: ["testError"],
+export const toFHIRFormat: TOperation<{
+  resource: any;
+}> = {
+  method: "POST",
+  path: ["$to-format", "fhir"],
   handlerFn: async (req, { ctx }) => {
-    switch (req.params.type) {
-      case "ValidationError":
-        throw new ValidationError("Testing ValidationError");
-      case "NotFoundError":
-        throw new NotFoundError("Something", {
-          foo: "foo",
-          bar: "bar",
-        });
-      case "AxiosError":
-        await ctx.request({ url: "http://xxx" });
-        return {};
-      default:
-        throw new Error("Testing default error");
-    }
-  },
-};
+    const { resource } = req;
+    assert.ok(resource, new ValidationError("resource required"));
 
-export const testApi: TOperation<{ params: { type: string } }> = {
-  method: "GET",
-  path: ["testApi"],
-  handlerFn: async (req, { ctx }) => {
-    const { resources: patients } = await ctx.api.findResources<any>("Patient");
+    const fhirResource = fromFirstClassExtension(resource);
 
-    const patient = !patients.length
-      ? null
-      : await ctx.api.getResource<any>("Patient", patients[0].id);
-
-    console.log({ patients, patient });
-    return { resource: { patients, patient } };
-  },
-};
-
-export const testPsql: TOperation = {
-  method: "GET",
-  path: ["test-psql"],
-  handlerFn: async (req, { ctx }) => {
-    const result = await ctx.psql("select * from attribute limit 1");
-    return { resource: result };
-  },
-};
-
-export const testSql: TOperation = {
-  method: "GET",
-  path: ["test-sql"],
-  handlerFn: async (req, { ctx }) => {
-    const result = await ctx.sql(
-      "select * from attribute where resource->>'module' = ? limit 1",
-      ["fhir-4.0.0"]
-    );
-    console.log(result);
-    return { resource: result };
-  },
-};
-
-export const testBundle: TOperation = {
-  method: "GET",
-  path: ["test-bundle"],
-  handlerFn: async (req, { ctx }) => {
-    const result = await ctx.api.createBundle("batch", [
-      {
-        request: { method: "POST", url: "/Patient" },
-        resource: { resourceType: "Patient" },
-      },
-    ]);
-    console.log(result);
-    return { resource: result };
+    return { resource: {resource: fhirResource}, status: 200 };
   },
 };
